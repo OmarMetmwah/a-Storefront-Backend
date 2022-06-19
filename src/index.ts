54069ba87e6213d091d4adb4b7ca32200be59cb9
OmarMetmwah <omarelbrns29@gmail.com>
2022-06-19 16:55:53 +0200

    init project dependencies

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express()
+const address: string = "localhost:3000"
+
+
+app.get('/', function (req: Request, res: Response) {
+    res.send('Hello World!!')
+})
+
+app.listen(3000, function () {
+    console.log(`starting app on: ${address}`)
+})
